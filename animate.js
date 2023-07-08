3496352b2b819e2036d159c6e6daa3c6ec64f520
Dawood-Ahmed19 <gfxwork66@gmail.com>
2023-07-08 14:42:54 +0500

    feat: added responsiveness

--- animate.js
+++ animate.js
@@ -1,15 +1,25 @@
-window.addEventListener("scroll", zoomOnScroll);
+// window.addEventListener("scroll", zoomOnScroll);
 
-function zoomOnScroll() {
-  var zoomElement = document.querySelector(".card");
-  var zoomElementTwo = document.querySelector(".card02");
-  var containerTop = zoomElement.getBoundingClientRect().top;
-  var containerBottom = zoomElement.getBoundingClientRect().bottom;
+// function zoomOnScroll() {
+//   var zoomElement = document.querySelector(".card");
+//   var zoomElementTwo = document.querySelector(".skills-section");
+//   var elementTop = zoomElementTwo.getBoundingClientRect().top;
+//   var elementBotton = zoomElementTwo.getBoundingClientRect().bottom;
+//   var animateIn = document.querySelector("skills-col02");
+//   var containerTop = zoomElement.getBoundingClientRect().top;
+//   var containerBottom = zoomElement.getBoundingClientRect().bottom;
 
-  // Check if the container is in the viewport
-  if (containerTop < window.innerHeight && containerBottom >= 0) {
-    zoomElement.classList.remove("zoomIn");
-  } else {
-    zoomElement.classList.add("zoomIn");
-  }
-}
+//   // First Animation
+//   if (containerTop < window.innerHeight && containerBottom >= 0) {
+//     zoomElement.classList.remove("zoomIn");
+//   } else {
+//     zoomElement.classList.add("zoomIn");
+//   }
+//   // Next Animation
+
+//   if (elementTop < window.innerHeight && elementBotton >= 0) {
+//     animateIn.classList.remove("animate");
+//   } else {
+//     animateIn.classList.add("animate");
+//   }
+// }
