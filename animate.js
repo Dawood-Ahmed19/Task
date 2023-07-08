52d50b0b1d93627f201395d3977c9485ff37025d
Dawood-Ahmed19 <gfxwork66@gmail.com>
2023-07-08 18:18:01 +0500

    feat: added scratch animations on scroll

--- animate.js
+++ animate.js
@@ -1,25 +1,18 @@
-// window.addEventListener("scroll", zoomOnScroll);
+window.addEventListener("scroll", onScroll);
 
-// function zoomOnScroll() {
-//   var zoomElement = document.querySelector(".card");
-//   var zoomElementTwo = document.querySelector(".skills-section");
-//   var elementTop = zoomElementTwo.getBoundingClientRect().top;
-//   var elementBotton = zoomElementTwo.getBoundingClientRect().bottom;
-//   var animateIn = document.querySelector("skills-col02");
-//   var containerTop = zoomElement.getBoundingClientRect().top;
-//   var containerBottom = zoomElement.getBoundingClientRect().bottom;
+function onScroll() {
+  var rotateElement = document.querySelector(".rotationalImage");
+  var elementTop = document.querySelector(".portfolio--section");
+  var containerTop = elementTop.getBoundingClientRect().top;
+  var containerBottom = elementTop.getBoundingClientRect().bottom;
 
-//   // First Animation
-//   if (containerTop < window.innerHeight && containerBottom >= 0) {
-//     zoomElement.classList.remove("zoomIn");
-//   } else {
-//     zoomElement.classList.add("zoomIn");
-//   }
-//   // Next Animation
+  // First Animation
+  if (containerTop < window.innerHeight && containerBottom >= 0) {
+    rotateElement.classList.add("activeImage");
+  } else {
+    rotateElement.classList.remove("activeImage");
+  }
+  // Next Animation
+}
 
-//   if (elementTop < window.innerHeight && elementBotton >= 0) {
-//     animateIn.classList.remove("animate");
-//   } else {
-//     animateIn.classList.add("animate");
-//   }
-// }
+console.log(onScroll());
